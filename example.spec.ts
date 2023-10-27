ea61c26f26b51ac955229118dafde6c87a4a114f
SLVGRM <slava.larionov@gmail.com>
2023-10-28 00:44:35 +0500

    refactoring

--- example.spec.ts
+++ example.spec.ts
@@ -1,25 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { mainPage, SearchInput, SubmitButton } from './mainPage';
+import {stub} from './askQuestionPage';
+import {previousQuarter, nextQuarter, monthInfoDays, monthInfoHours} form '/.calendarPage'
 
 test('Searching from main', async ({ page }) => {
-  await page.goto('https://normativ.kontur.ru/');
-  await  page.locator("[data-tid='SearchInput']").fill("налог");
-  await (await page.locator("[data-tid='SearchSubmitButton']")).click();
+  await page.goto(mainPage);
+  await SearchInput.fill("налог");
+  await SubmitButton.click();
   await expect(page).toHaveURL(/.*searching=true&sortby=1&searchquerysource=2&from=Main/);
 });
 
 test('Stub in ask-question', async ({ page }) => {
-  await page.goto('https://normativ.kontur.ru/consult/ask-question');
-  await  page.locator("[data-tid='UnAuthorizedStub']");
-  var stub = page.locator("[data-tid='UnAuthorizedStub']");
+  await page.goto(askQuestionPage);
   await expect(stub).toBeVisible;
 });
 
 test('Month info is visible', async ({ page }) => {
-  await page.goto('https://normativ.kontur.ru/buhcalendar/2023/10/30?from=mainPageHeader');
-  await  page.locator('.calendar-prev').click();
-  await  page.locator('.calendar-next').click();
-  var monthInfoDays = page.locator("[data-tid='BuhCalendarMonthInfoDays']")
-  var monthInfoHours = page.locator("[data-tid='BuhCalendarMonthInfoHours']")
+  await page.goto(calendarPage);
+  await  previousQuarter.click();
+  await nextQuarter.click();
   await expect(monthInfoDays).toHaveText('22рабочих дня');
   await expect(monthInfoHours).toHaveText('176рабочих часов');
 });

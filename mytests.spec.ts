a4eebaa94be03394d88bf67e30fca8c237601695
SLVGRM <slava.larionov@gmail.com>
2023-10-28 01:49:32 +0500

    fix

--- mytests.spec.ts
+++ mytests.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
-import { mainPage, SearchInput, SubmitButton } from './mainPage';
+import { mainPageUrl, SearchInput, SubmitButton } from './mainPage';
 import {stub} from './askQuestionPage';
-import {previousQuarter, nextQuarter, monthInfoDays, monthInfoHours} form '/.calendarPage'
+import {previousQuarter, nextQuarter, monthInfoDays, monthInfoHours} from './calendarPage'
 
 test('Searching from main', async ({ page }) => {
-  await page.goto(mainPage);
+  await page.goto(mainPageUrl);
   await SearchInput.fill("налог");
   await SubmitButton.click();
   await expect(page).toHaveURL(/.*searching=true&sortby=1&searchquerysource=2&from=Main/);

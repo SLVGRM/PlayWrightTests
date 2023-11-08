d9471bf029d498d764f8cc5d088e94083ca252d0
SLVGRM <slava.larionov@gmail.com>
2023-11-09 00:35:36 +0500

    change timezone

--- MainPageTests/access.spec.ts
+++ MainPageTests/access.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { mainPage } from './mainPage';
 
+test.use({timezoneId: 'Europe/Paris'});
 test('Searching from main', async ({ page }) => {
     await page.goto(mainPage.mainPageUrl);
     await  page.locator(mainPage.searchInput).fill("налог");
